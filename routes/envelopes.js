4b30372b0d21ac845454877bbe66b95b2304ccd9
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-04-28 14:51:02 -0400

    feat(envelopes): add GET and POST routes for managing envelopes

--- routes/envelopes.js
+++ routes/envelopes.js
@@ -1,3 +1,28 @@
 const express = require("express");
-const envelopes = express.Router();
-const { envelopes } = require("../db.js");
+const envelopeRouter = express.Router();
+const { mockEnvelopes } = require("../db.js");
+
+envelopeRouter.get("/", (req, res, next) => {
+  res.send(mockEnvelopes);
+});
+
+envelopeRouter.post("/", (req, res, next) => {
+  //finds the highest ID in the mockEnvelopes array
+  const findHighestId = mockEnvelopes.reduce((acc, currentVal) => {
+    if (acc.id > currentVal.id) {
+      return acc.id;
+    } else {
+      return currentVal.id;
+    }
+  }, 0); // Starts at 0 to handle an empty array
+  const newBudget = {
+    id: findHighestId + 1,
+    title: req.body.title,
+    budget: req.body.budget,
+  };
+  mockEnvelopes.push(newBudget);
+
+  res.status(201).send(mockEnvelopes);
+});
+
+module.exports = envelopeRouter;
