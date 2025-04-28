9c4cee41f50668d404a379a154bd13480e90d0c6
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-04-28 12:26:24 -0400

    feat(server.js): import mock data from db.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3005;
+const { envelopes } = require("db.js");
+
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
